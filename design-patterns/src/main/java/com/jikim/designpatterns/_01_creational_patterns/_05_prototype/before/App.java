36f53966e0d59c0369cae1c1aad0de12ca54cb4d
kho903 <gmldnr2222@naver.com>
2022-07-04 14:27:49 +0900

    Design patterns / 01. Creational Pattern / 05. 프로토타입 패턴 / 01. 패턴 소개

---
+++ design-patterns/src/main/java/com/jikim/designpatterns/_01_creational_patterns/_05_prototype/before/App.java
@@ -0,0 +1,24 @@
+package com.jikim.designpatterns._01_creational_patterns._05_prototype.before;
+
+public class App {
+	public static void main(String[] args) {
+		GithubRepository repository = new GithubRepository();
+		repository.setUser("whiteShip");
+		repository.setName("live-study");
+
+		GithubIssue githubIssue = new GithubIssue(repository);
+		githubIssue.setId(1);
+		githubIssue.setTitle("1주차 과제: JVM은 무엇이며 자바 코드는 어떻게 실행하는 것인가.");
+
+		String url = githubIssue.getUrl();
+		System.out.println(url);
+
+		/*GithubIssue githubIssue2 = new GithubIssue(repository);
+		githubIssue2.setId(2);
+		githubIssue2.setTitle("2주차 과제: ...");*/
+
+		// GithubIssue clone = githubIssue.clone();
+		// clone != githubIssue
+		// clone.equals(githubIssue) => true
+	}
+}
